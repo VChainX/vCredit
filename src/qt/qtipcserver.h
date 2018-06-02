ccb21120d25148347cfb3a1e64696750d5287e82
VCX vCredit <39309604+VChainX@users.noreply.github.com>
2018-06-03 02:16:53 +0800

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define vcredit-qt message queue name
+#define BITCOINURI_QUEUE_NAME "vcreditURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
